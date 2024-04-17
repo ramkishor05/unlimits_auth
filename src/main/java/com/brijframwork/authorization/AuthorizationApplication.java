4400beb5c292c61c9bd5e9d03563718526eefef4
Ram Kishor <krishnaenterprises0423@gmail.com>
2024-04-17 09:31:26 +0530

    ok

--- src/main/java/com/brijframwork/authorization/AuthorizationApplication.java
+++ src/main/java/com/brijframwork/authorization/AuthorizationApplication.java
@@ -1,5 +1,6 @@
 package com.brijframwork.authorization;
 
+import org.brijframework.bean.builder.BeanBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,10 @@ public class AuthorizationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationApplication.class, args);
+		
+		BeanBuilder builder=new BeanBuilder(args);
+		
+		builder.set
 	}
 
 }
